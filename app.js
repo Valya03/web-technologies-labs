d3c253c8948ea5bfff34ba90e9b0073a631da075
Bashcony <dbarov3@gmail.com>
2024-04-19 14:03:15 +0500

    lab2

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
-const audio = document.querySelector('#audio');
-const btnka = document.querySelector('#btnka');
+var main_id = document.querySelector("main").id;
+console.log(main_id)
+var navmenu = document.getElementsByClassName("nav")[0]
+console.log(navmenu)
+var naventity = navmenu.getElementsByTagName("a");
+console.log(naventity)
+var menuelem;
 
-btnka.addEventListener('click', () => {
-    audio.paused ? audio.play() : audio.pause();
+switch(main_id) {
+    case 'index':
+        menuelem = naventity[0];
+        break;
+    case 'menu':
+        menuelem = naventity[1];
+        break;
+    case 'about_us':
+        menuelem = naventity[2];
+        break;
+    case 'booking':
+        menuelem = naventity[3];
+        break;
+    default:
+        menuelem = naventity[0];
+}
+console.log(menuelem)
+naventity.innerHTML = naventity[0].innerHTML;
+menuelem.classList.add("active");
+
+$(document).ready(function() {
+    $(".hidebox p").hide();
 });
+$(".hidebox h2").click(function () {
+    $(this).next("p").hide("slow");
+    return false;
+}).dblclick(function () {
+    $(this).next("p").show("slow");
+    return false;
+});
+
+// $('.hidebox h2').click(function() {
+//     var boxText = $('.hidebox p').is(":visible") ? "Show me" : "Hide me";
+//     $(".hidebox h2").suffix(boxText);
+//     $('.hidebox p').slideToggle(250);
+//     return false;
+// });
